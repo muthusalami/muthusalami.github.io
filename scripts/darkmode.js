f61b6a33a8f49f7b646fbb03417698a9ee8e0d2d
Matthew Yang <matthewyangjiefeng@gmail.com>
2024-08-20 22:12:06 -0400

    cleaned up darkmode.js

--- scripts/darkmode.js
+++ scripts/darkmode.js
@@ -2,10 +2,6 @@ document.addEventListener("DOMContentLoaded", () => {
   let darkMode = localStorage.getItem("darkMode");
   const darkModeToggle = document.querySelector("#dark-mode-toggle");
 
-  // check if dark mode is enabled
-  // if enabled, turn it off
-  // if it's disabled, turn it on
-
   const enableDarkMode = () => {
     // add class dark mode to the body
     document.body.classList.add("darkmode");
